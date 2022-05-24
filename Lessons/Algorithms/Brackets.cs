a58ab1ec60f9ea0d39512ec343704b5b10e5b245
danyelchidera <danyelchidera1@gmail.com>
2022-05-24 11:11:30 +0100

    included brackets algo

---
+++ Lessons/Algorithms/Brackets.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons.Algorithms
+{
+    public class Brackets
+    {
+        public bool IsBracketsValid(string str)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            var isValid = true;
+            foreach (var i in str)
+            {
+                switch (i)
+                {
+                    case '{':
+                        stack.Push(i);
+                        break;
+                    case '(':
+                        stack.Push(i);
+                        break;
+                    case '<':
+                        stack.Push(i);
+                        break;
+                    case '[':
+                        stack.Push(i);
+                        break;
+                    case '}':
+                        {
+                            if (stack.Pop() != '{')
+                            {
+                                return false;
+                            }
+                        }
+                        break;
+                    case ')':
+                        {
+                            if (stack.Pop() != '(')
+                            {
+                                return false;
+                                
+
+                            }
+                            break;
+                        }
+                    case ']':
+                        {
+                            if (stack.Pop() != '[')
+                            {
+                                return false;
+
+                            }
+                            break;
+                        }
+                    case '>':
+                        {
+                            if (stack.Pop() != '<')
+                            {
+                                return false;
+
+                            }
+                            break;
+                        }
+                }
+            }
+
+            return true;
+        }
+    }
+}
