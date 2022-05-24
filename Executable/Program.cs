90f5e6143b610fe60baaa5af762e2a0b8b4ff972
danyelchidera <danyelchidera1@gmail.com>
2022-05-24 09:27:29 +0100

    First Commit

---
+++ Executable/Program.cs
@@ -0,0 +1,34 @@
+﻿using Lessons.Delegates;
+using Lessons.Models;
+
+List<Employee> emp = new List<Employee>();
+emp.Add(new Employee
+{
+    Id = 1,
+    Name = "John Obi",
+    Age = 5
+});
+
+emp.Add(new Employee
+{
+    Id = 2,
+    Name = "Alex",
+    Age = 89
+});
+
+emp.Add(new Employee
+{
+    Id = 3,
+    Name = "Daniel Codes",
+    Age = 7
+});
+
+
+
+var t = emp.MyWhereVersion(emp => emp.Id > 1);
+
+foreach(Employee e in t)
+{
+    Console.WriteLine(e.Name);
+}
+
---
+++ Lessons/Practice/Delegates/Lambda.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons.Delegates
+{
+    public static class Lambda
+    {         
+        public static IEnumerable<T> MyWhereVersion<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            foreach(var item in source)
+            {
+                if(predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
