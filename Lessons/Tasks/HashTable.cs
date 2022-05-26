a1b766d75adddce07008412dfbbba8de6407b537
danyelchidera <danyelchidera1@gmail.com>
2022-05-26 18:57:24 +0100

    hash table

--- Lessons/Tasks/HashTable.cs
+++ Lessons/Tasks/HashTable.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Lessons.Tasks
+﻿namespace Lessons.Tasks
 {
-    internal class HashTable
+    public class HashTable<T, U>
     {
+        private class Entry
+        {
+            public T key;
+            public U value;
+        }
+
+        LinkedList<Entry>[] entries = new LinkedList<Entry>[10];
+
+        public void Add(T key, U vlaue)
+        {
+            var hash = HashFunction(key);
+        }
+
+        public int HashFunction(T key)
+        {
+            var hash = key.GetHashCode();
+            if(hash < 0)
+            {
+                hash *= -1;
+            }
+
+            return hash % 10;
+        }
     }
 }
